e2885145b1666b14e82d7f0d7c9a3076dfb7064c
Joe Mwamba <joemwambakas@gmail.com>
2025-12-07 16:24:14 -0500

    Ajout du script français et installateur

---
+++ verif-site.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# =====================================
+# Installeur automatique – Site Checker FR
+# =====================================
+
+echo -e "\n🌐 Téléchargement du script depuis GitHub..."
+curl -L -o ~/verif-site.sh https://raw.githubusercontent.com/mwamba-kas/site-checker-fr/main/verif-site.sh
+
+echo -e "\n⚡ Rendre le script exécutable..."
+chmod +x ~/verif-site.sh
+
+echo -e "\n🚀 Lancement du script..."
+~/verif-site.sh
